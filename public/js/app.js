5aa93a07b6af2712a7ea599e2da36c7b727075a6
kollynce <collinsodeny@gmail.com>
2019-10-24 14:52:24 +0300

    Added logo

--- public/js/app.js
+++ public/js/app.js
@@ -105932,8 +105932,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\writing-technical\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\writing-technical\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/kollynce/Documents/writing-technical/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/kollynce/Documents/writing-technical/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
